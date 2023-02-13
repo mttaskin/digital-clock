556770e6624ed779d9b18eaa3345515bec8deda7
mttaskin <0mttaskin1@gmail.com>
2023-02-13 16:47:57 +0300

    js second part

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ setInterval(() => {
   const b = Math.floor(Math.random() * 255);
   const c = Math.floor(Math.random() * 255);
   const renk = `rgb(${a},${b},${c})`;
+  
+  const time = new Date();
+  
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  let day = time.getDay();
+  const gun = days[day];
+
+  let date = time.getDate();
+  let month = time.getMonth() + 1;
+  let year = time.getFullYear();
