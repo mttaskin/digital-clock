6f1bd775b3797f271b9a41499fccd440cf78de07
mttaskin <0mttaskin1@gmail.com>
2023-02-13 16:48:59 +0300

    project done

--- app.js
+++ app.js
@@ -26,3 +26,22 @@ setInterval(() => {
   let date = time.getDate();
   let month = time.getMonth() + 1;
   let year = time.getFullYear();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  date = date < 10 ? "0" + date : date;
+  month = month < 10 ? "0" + month : month;
+
+  const timeDiv = document.querySelector("#time span");
+  timeDiv.innerText = hours + " : " + minutes + " : " + seconds;
+  timeDiv.style.color = `${renk}`;
+
+  const dateDiv = document.querySelector("#date");
+  dateDiv.innerText = date + " / " + month + " / " + year;
+
+  const dayDiv = document.querySelector("#day");
+  dayDiv.innerText = gun;
+
+}, 1000);
